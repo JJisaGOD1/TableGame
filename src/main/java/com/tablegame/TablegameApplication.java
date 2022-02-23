156cbf16746b7155a6ce822123f571a7eb2fae5a
Chun <z842657913@yahoo.com.tw>
2022-02-23 16:37:25 +0800

    first commit

---
+++ src/main/java/com/tablegame/TablegameApplication.java
@@ -0,0 +1,14 @@
+package com.tablegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablegameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablegameApplication.class, args);
+		System.out.println("小組公用桌遊專案!");
+	}
+
+}
