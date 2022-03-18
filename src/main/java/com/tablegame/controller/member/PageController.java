09bb3cccbbb563ba95bba92c3a8bb3ccd2757943
Ray <chrayray@gmail.com>
2022-03-18 09:16:30 +0800

    Controller Bug修復

--- src/main/java/com/tablegame/controller/member/PageController.java
+++ src/main/java/com/tablegame/controller/member/PageController.java
@@ -202,16 +202,14 @@ public class PageController {
 	}
 
 	@GetMapping(value = "/query")
-
 	public ModelAndView query(ModelAndView mav, @RequestParam(name = "id") Integer id) {
-
 		MembersBean nowCostomer = service.findMemberById(id); // 重資料庫撈新資料
 
-	public ModelAndView query(ModelAndView mav, HttpSession session) {
-		MembersBean logCustomer = (MembersBean) session.getAttribute("member");
-		Integer loginCustomerId = logCustomer.getId();
-
-		MembersBean nowCostomer = service.findMemberById(loginCustomerId); // 重資料庫撈新資料
+//	public ModelAndView query(ModelAndView mav, HttpSession session) {
+//		MembersBean logCustomer = (MembersBean) session.getAttribute("member");
+//		Integer loginCustomerId = logCustomer.getId();
+//
+//		MembersBean nowCostomer = service.findMemberById(loginCustomerId); // 重資料庫撈新資料
 
 
 //		System.out.println(logCustomer.getCusName()); ////更正後Session為舊資料
@@ -221,7 +219,7 @@ public class PageController {
 	}
 //修改OK
 	@PostMapping("/querySubmit")
-	public String querySubmit(@Valid @ModelAttribute(name = "query") MembersBean bean, Model model, HttpSession hs) {-
+	public String querySubmit(@Valid @ModelAttribute(name = "query") MembersBean bean, Model model, HttpSession hs) {
 		
 		MembersBean res=service.insertMember(bean);
 		
